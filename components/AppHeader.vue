989d8125e801d70de13e28503233757baa54a65a
sidczak <sidczak@clickmeeting.com>
2022-11-28 21:58:54 +0100

    layout pages

---
+++ components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Header</h1>
+  <NuxtLink to="/">Start</NuxtLink>
+  <NuxtLink to="/about"> about</NuxtLink>
+  <br />
+  <br />
+  <button @click="$router.push('/')">Start</button>
+  <button @click="$router.push('/about')">About</button>
+  Current route: <code>{{ route.path }}</code>
+</template>
+<script setup>
+const route = useRoute();
+</script>
