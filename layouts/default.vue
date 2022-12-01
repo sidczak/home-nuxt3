0cee8b7cd1f8893bf051c88724b41ef0a8f55203
sidczak <sidczak@clickmeeting.com>
2022-12-01 22:38:54 +0100

    add assets

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,12 @@
-<template>
-  <AppHeader />
-  <slot />
-  <AppFooter />
+<template lang="pug">
+div.bg-clouds-concave-145
+  | bg-clouds-concave-145
+AppHeader
+slot
+AppFooter.red
 </template>
+<style lang="scss" scoped>
+  .red {
+    border: 1px solid $turquoise;
+  }
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  css: ["@/assets/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/variables/_colors.scss" as *;'
+        }
+      }
+    }
+  }
 })
