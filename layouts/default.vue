9bd3c27e41d21c0e2544d4fa9caf0947fb764225
sidczak <sidczak@clickmeeting.com>
2022-12-02 00:28:05 +0100

    add color mode modules

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,17 @@
 <template lang="pug">
 div.bg-clouds-concave-145
   | bg-clouds-concave-145
+h1 
+  | Color mode: {{ $colorMode.value }}
+select(v-model="$colorMode.preference")
+  option(value="system")
+    | System
+  option(value="light")
+    | Light
+  option(value="dark")
+    | Dark
+  option(value="sepia")
+    | Sepia
 AppHeader
 slot
 AppFooter.red
